2ffd7a56e964cd3b60ed968fd29459c644f4ba78
Rasmus Eskola <fruitiex@gmail.com>
2018-04-28 22:40:13 +0300

    Implement scenes

--- src/plugins/scene.ts
+++ src/plugins/scene.ts
@@ -1,19 +1,63 @@
 import * as Koa from 'koa';
-import { Scene } from '../types';
+import { Scene, LuminaireUpdateFields, StateType, HSVState } from '../types';
+import { updateLuminaires } from '../core/luminaire';
+import { convertTo } from '../utils';
+import { groupExists, getGroup } from './group';
+import { flatten } from 'ramda';
 
-interface Options {}
+type Options = Scene[];
 
 interface State {
   app?: Koa;
-  scenes: {
-    [id: string]: Scene;
-  };
+  scenes: Scene[];
 }
 
-const state: State = {
-  scenes: {},
+const state: State = { scenes: [] };
+
+/**
+ * Activates a scene by id. Throws if scene not found.
+ */
+const activateScene = (id: string) => {
+  const scene = state.scenes.find(scene => scene.id === id);
+
+  if (!scene) throw new Error(`Scene with id '${id}' not found`);
+
+  // TODO: get rid of janky type assertions
+  const fieldsList = <LuminaireUpdateFields[]>flatten(
+    scene.targets.map(target => {
+      const brightness = target.brightness || 1;
+
+      const colors = (target.colors || scene.colors)
+        .map(color => <HSVState>convertTo(color, StateType.HSV))
+        .map(color => ({ ...color, v: color.v * brightness }));
+
+      const effects = target.effects || scene.effects;
+
+      let fields = {
+        id: target.id,
+        colors,
+        effects,
+      };
+
+      if (groupExists(target.id)) {
+        // If this is a group, return an array of fields (one element for each
+        // luminaire)
+        const group = getGroup(target.id);
+
+        return group.luminaires.map(luminaireId => ({
+          ...fields,
+          id: luminaireId,
+        }));
+      } else {
+        return fields;
+      }
+    }),
+  );
+
+  updateLuminaires(fieldsList);
 };
 
 export const register = async (app: Koa, options: Options) => {
   state.app = app;
+  state.scenes = options;
 };
