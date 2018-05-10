3d6c9a8701adae79e87be4439e4af5a5a9851313
Rasmus Eskola <fruitiex@gmail.com>
2018-05-10 16:21:06 +0300

    WIP: nightlight, resize effects

---
+++ src/plugins/effects/nightlight.ts
@@ -0,0 +1,20 @@
+import { HSVState, EffectOptions } from '../../types';
+
+interface State {
+  luminairesToFade: string[];
+}
+
+const state: State = {
+  luminairesToFade: [],
+};
+
+export default (
+  colors: HSVState[],
+  options: EffectOptions,
+  luminaireId: string,
+  numLightSources: number,
+  effectIndex: number,
+  numEffects: number,
+): HSVState[] => {
+  return colors;
+};
