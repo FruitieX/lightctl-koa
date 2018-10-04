d6646efc837b7b0c27cac555c72fb12a469a9453
Rasmus Eskola <fruitiex@gmail.com>
2018-10-04 20:51:16 +0300

    Implement basic REST API for fetching luminaire states

--- src/plugins/api/v1.ts
+++ src/plugins/api/v1.ts
@@ -1,7 +1,11 @@
 import * as Koa from 'koa';
 import * as Router from 'koa-router';
 import * as bodyParser from 'koa-bodyparser';
-import { updateLuminaires } from '../../core/luminaire';
+import {
+  updateLuminaires,
+  getLuminaires,
+  getLuminaire,
+} from '../../core/luminaire';
 import { activateScene } from '../../core/scene';
 
 interface Options {
@@ -25,17 +29,25 @@ const activateScene_: Router.IMiddleware = (ctx, next) => {
   ctx.body = activateScene(ctx.params.id);
 };
 
+const getLuminaires_: Router.IMiddleware = (ctx, next) => {
+  ctx.body = getLuminaires();
+};
+
+const getLuminaire_: Router.IMiddleware = (ctx, next) => {
+  ctx.body = getLuminaire(ctx.params.id);
+};
+
 export const register = async (app_: Koa, options: Options) => {
   const app = new Koa();
-  const router = new Router();
+  const router = new Router({ prefix: '/api/v1' });
 
   app.use(bodyParser());
   app.use(router.routes()).use(router.allowedMethods());
 
   router.patch('updateLuminaire', '/luminaires/:id', updateLuminaire_);
   router.post('activateScene', '/scenes/:id', activateScene_);
-  //router.get('luminaires', '/luminaires', getLuminaires_);
-  //router.get('luminaire', '/luminaires/:id', getLuminaire_);
+  router.get('luminaires', '/luminaires', getLuminaires_);
+  router.get('luminaire', '/luminaires/:id', getLuminaire_);
 
   const port = options.port || 5678;
   app.listen(port);
